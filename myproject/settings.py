209d34ad39214b8f8b6297f39dcf257f030190f5
James Whitby <jameswhitby813@gmail.com>
2022-12-31 23:32:28 +0000

    should do the trick

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*5=7pi&)4)5&gt(84r7&jcal-)1(0k=&b&mfo@7&mbkts+@&7!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
